a867c65da7e8f81b7a67f46b92643bbaea7b081c
Leonid Bloch <leonid.bloch@esrf.fr>
2017-01-02 01:43:13 +0200

    Some refactoring, style, and fool-proofing fixes
    Signed-off-by: Leonid Bloch <leonid.bloch@esrf.fr>

--- compile_all.sh
+++ compile_all.sh
@@ -3,13 +3,13 @@
 ### CHANGE THESE ###
 EXP="ma2916"
 BINNINGS=( 0.001 0.002 0.003 )
-TEMPRES_PATH=$HOME"/users/"$EXP"/id22/temp.res"
+TEMPRES_PATH="${HOME}/users/${EXP}/id22/temp.res"
 
 ### CHANGE ONLY IF NEEDED ###
 SUM_BIN_PATH="id31sum"
 SUM_BIN_EXTRA_PARAMS="lowtth=0.5 scalmon gsas"
-DAT_DIR=$HOME"/data1"
-DEST_DIR=$HOME"/users/"$EXP"/id22/ALL_AUTOCOMPILED"
+DAT_DIR="${HOME}/data1"
+DEST_DIR="${HOME}/users/${EXP}/id22/ALL_AUTOCOMPILED"
 
 ### DO NOT CHANGE FROM HERE ###
 getscans() {
@@ -17,7 +17,7 @@ getscans() {
     local __IND_ENTRIES=$3
     local SCANS=($(grep "#S" $1 | awk '{print $2}'))
     local ENTRIES=${#SCANS[@]}
-    for e in $(seq 1 $(($ENTRIES-1)))
+    for e in $(seq 1 $(($ENTRIES - 1)))
     do
         if [ ${SCANS[$e]} -ne $((${SCANS[$e-1]}+1)) ]
         then
@@ -33,10 +33,10 @@ makedata() {
     for b in ${BINNINGS[@]}
     do
         echo "--- Binning $b ---"
-        mkdir -p $DEST_DIR/$BASENAME/$1/bin_$b
-        cp $TEMPRES_PATH $DEST_DIR/$BASENAME/$1/bin_$b/
-        pushd $DEST_DIR/$BASENAME/$1/bin_$b
-        $SUM_BIN_PATH ../../$FILENAME $b $2 $3 ${SUM_BIN_EXTRA_PARAMS}
+        mkdir -p "${DEST_DIR}/${BASENAME}/${1}/bin_${b}"
+        cp "${TEMPRES_PATH}" "${DEST_DIR}/${BASENAME}/${1}/bin_${b}/"
+        pushd "${DEST_DIR}/${BASENAME}/${1}/bin_${b}"
+        ${SUM_BIN_PATH} "../../${FILENAME}" $b $2 $3 ${SUM_BIN_EXTRA_PARAMS}
         popd
     done
 }
@@ -47,14 +47,14 @@ do
     FILENAME=${f##*/}
     BASENAME=${FILENAME%.*}
     echo "------------------------------"
-    echo "Processing $BASENAME ..."
+    echo "Processing ${BASENAME} ..."
     echo "------------------------------"
     getscans $f IND_SCANS ENT
     SCANS=(${IND_SCANS})
     FIRST=${SCANS[0]}
-    LAST=${SCANS[$ENT-1]}
-    mkdir -p $DEST_DIR/$BASENAME
-    cp $f $DEST_DIR/$BASENAME/
+    LAST=${SCANS[${ENT}-1]}
+    mkdir -p "${DEST_DIR}/${BASENAME}"
+    cp "$f" "${DEST_DIR}/${BASENAME}/"
     for s in ${IND_SCANS[@]}
     do
         echo "##### Scan $s #####"
